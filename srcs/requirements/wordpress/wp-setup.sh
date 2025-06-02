7add0d42297a82304b8a862529c8ed834273362a
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-06-02 21:40:31 +0100

    finished setup

--- srcs/requirements/wordpress/wp-setup.sh
+++ srcs/requirements/wordpress/wp-setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 WP_CONFIG_SAMPLE="/var/www/wordpress/wp-config-sample.php"
+WP_CONFIG="/var/www/wordpress/wp-config.php"
 
-# Replace placeholders with real values in the config file
-sed -i "s/database_name_here/${MYSQL_DATABASE}/" "$WP_CONFIG_SAMPLE"
-sed -i "s/username_here/${MYSQL_USER}/" "$WP_CONFIG_SAMPLE"
-sed -i "s/password_here/${MYSQL_PASSWORD}/" "$WP_CONFIG_SAMPLE"
-sed -i "s/localhost/${MYSQL_HOST}/" "$WP_CONFIG_SAMPLE"
+# Replace placeholders with real environment variables
+sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" "$WP_CONFIG_SAMPLE"
+sed -i "s/username_here/${WORDPRESS_DB_USER}/" "$WP_CONFIG_SAMPLE"
+sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" "$WP_CONFIG_SAMPLE"
+
+# Strip port from host if present (e.g., mariadb-container:3306 -> mariadb-container)
+DB_HOST=${WORDPRESS_DB_HOST%%:*}
+sed -i "s/localhost/${DB_HOST}/" "$WP_CONFIG_SAMPLE"
+
+# Rename the sample config file to wp-config.php
+mv "$WP_CONFIG_SAMPLE" "$WP_CONFIG"
+
