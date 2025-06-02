a587ad7aad1384ade2580ca35e2c8c1e8a8a7eee
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-06-02 23:39:25 +0100

    aadd script for connect to wordpress

--- srcs/requirements/wordpress/wp-setup.sh
+++ srcs/requirements/wordpress/wp-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-WP_CONFIG_SAMPLE="/var/www/wordpress/wp-config-sample.php"
-WP_CONFIG="/var/www/wordpress/wp-config.php"
+WP_PATH="/var/www/wordpress"
+WP_CONFIG_SAMPLE="$WP_PATH/wp-config-sample.php"
+WP_CONFIG="$WP_PATH/wp-config.php"
 
 # Replace placeholders with real environment variables
 sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" "$WP_CONFIG_SAMPLE"
@@ -15,3 +16,24 @@ sed -i "s/localhost/${DB_HOST}/" "$WP_CONFIG_SAMPLE"
 # Rename the sample config file to wp-config.php
 mv "$WP_CONFIG_SAMPLE" "$WP_CONFIG"
 
+# Wait until WordPress is ready
+until curl -s http://localhost | grep -q "WordPress"; do
+  echo "Waiting for WordPress to be ready..."
+  sleep 5
+done
+
+# Install WordPress using WP-CLI
+WP_PATH="/var/www/wordpress"
+
+# Install WordPress using WP-CLI
+# wp core install \
+#   --path="$WP_PATH" \
+#   --url="http://localhost:8080" \
+#   --title="inception" \
+#   --admin_user="admin" \
+#   --admin_password="admin123" \
+#   --admin_email="admin@admin.com" \
+#   --skip-email
+
+# # Optional: discourage search engines from indexing
+# wp option update blog_public 0 --path="$WP_PATH"
