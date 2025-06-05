a28d5fa5bb93a4801ca3642264246eaff7280480
ZAKARIAE <elkhadirzakaria48@gmail.com>
2025-06-05 19:28:02 +0100

    debug

--- srcs/requirements/nginx/generate-ssl.sh
+++ srcs/requirements/nginx/generate-ssl.sh
@@ -2,8 +2,4 @@
 
 
 
-openssl req -x509 -nodes -days 365 \
-  -newkey rsa:2048 \
-  -keyout /etc/nginx/ssl/privkey.pem \
-  -out /etc/nginx/ssl/cert.pem \
-  -subj "/C=US/ST=DevState/L=DevCity/O=DevOrg/CN=localhost"
+
