3f9eaa3feffc4372db944ba2a579570a3199c692
ZAKARIAE <elkhadirzakaria48@gmail.com>
2025-06-25 20:13:42 +0100

    fix https

--- srcs/requirements/wordpress/wp-setup.sh
+++ srcs/requirements/wordpress/wp-setup.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
-sed -i 's|^listen = .*|listen = 9000|' /etc/php/8.2/fpm/pool.d/www.conf
 mkdir -p /var/www/wordpress
 
-curl -o /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
-tar -xzf /tmp/wordpress.tar.gz -C /var/www
-rm /tmp/wordpress.tar.gz
-chown -R www-data:www-data /var/www/wordpress
+cd /var/www/wordpress
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wget -O wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+
+cp wp-config-sample.php wp-config.php
+
+sudo sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config.php 
+sudo sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config.php 
+sudo sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config.php 
+sudo sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb-container:3306' );/g" wp-config.php 
+
 
 echo "Waiting for MariaDB to be ready..."
-until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+until mysqladmin ping -h"mariadb-container" --silent; do
   sleep 2
 done
 
-WP_PATH="/var/www/wordpress"
-WP_CONFIG_SAMPLE="$WP_PATH/wp-config-sample.php"
-WP_CONFIG="$WP_PATH/wp-config.php"
-cp "$WP_CONFIG_SAMPLE" "$WP_CONFIG"
-sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" "$WP_CONFIG"
-sed -i "s/username_here/${WORDPRESS_DB_USER}/" "$WP_CONFIG"
-sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" "$WP_CONFIG"
-DB_HOST=${WORDPRESS_DB_HOST%%:*}
-sed -i "s/localhost/${DB_HOST}/" "$WP_CONFIG"
-
-echo "
-if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    \$_SERVER['HTTPS'] = 'on';
-}
-" >> "$WP_CONFIG"
-
 wp core install \
-  --path="$WP_PATH" \
+  --path="/var/www/wordpress" \
   --url="https://localhost" \
   --title="inception" \
   --admin_user="admin" \
@@ -42,3 +33,10 @@ wp core install \
   --admin_email="admin@admin.com" \
   --skip-email \
   --allow-root
+
+sudo chown -R www-data:www-data /var/www/wordpress
+sudo chmod -R 755 /var/www/wordpress
+
+sudo sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+
+php-fpm8.2 -F
